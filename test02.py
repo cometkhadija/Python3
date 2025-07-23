49634457155d1e5b7fc3678067eba5500b37a54a
Sayeda khadija Rahman <sayedakhadijarahman@gmail.com>
2025-07-23 22:11:45 +0600

    new commit for 23rd july

---
+++ test02.py
@@ -0,0 +1,15 @@
+# Numpy for images
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+img1 = plt.imread('C:/Users/CSELAB-2/Desktop/Python3/misc/4.1.07.tiff')
+print(type(img1))
+print(img1.shape)
+print(img1.ndim)
+print(img1.size)
+print(img1.dtype)
+print(img1.nbytes)
+
+
+
