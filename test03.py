109480e3f331ffe2380b2a00d4c021c5162035db
Sayeda khadija Rahman <sayedakhadijarahman@gmail.com>
2025-07-23 22:12:08 +0600

    new commit for 23rd july

---
+++ test03.py
@@ -0,0 +1,15 @@
+# Numpy for grey images
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+img1 = plt.imread('C:/Users/CSELAB-2/Desktop/Python3/misc/5.1.12.tiff')
+print(type(img1))
+print(img1.shape)
+print(img1.ndim)
+print(img1.size)
+print(img1.dtype)
+print(img1.nbytes)
+
+
+
