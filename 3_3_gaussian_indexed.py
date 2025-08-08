c503c8457c16d139b02d55e27a8ec880c165f8f7
Sayeda khadija Rahman <sayedakhadijarahman@gmail.com>
2025-08-08 15:08:32 +0600

    third lab

---
+++ 3_3_gaussian_indexed.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
+
+img = plt.imread('C:/Users/ASUS/Desktop/Python3/misc/indexed_GIF.gif')
+img_gaussian = gaussian_filter(img, sigma=3)
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+axes[0].imshow(img, cmap='gray')
+axes[0].set_title('Original Indexed')
+axes[0].axis('off')
+
+axes[1].imshow(img_gaussian, cmap='gray')
+axes[1].set_title('Gaussian Filtered (σ=3)')
+axes[1].axis('off')
+
+plt.tight_layout()
+plt.show()
